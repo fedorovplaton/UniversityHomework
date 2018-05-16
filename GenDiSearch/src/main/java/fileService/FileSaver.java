c1c9cc742600af451e41cf11810e659c179bb5c5
Timur Rakhimov <moldybizkit@mail.ru>
2018-05-16 11:18:23 +0300

    FileService has been added

--- GenDiSearch/src/main/java/fileService/FileSaver.java
+++ GenDiSearch/src/main/java/fileService/FileSaver.java
@@ -1,17 +1,20 @@
 package fileService;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Set;
 
 public class FileSaver {
     private String folder;
     private String fileName;
-    private HashMap<String,String> stat;
+    private HashMap<String,HashMap<String,String >> statistics;
 
-    public FileSaver(String folder, String fileName, HashMap<String, String> stat){
+    public FileSaver(String folder, String fileName, HashMap<String, HashMap<String,String >> statistics){
         this.folder=folder;
         this.fileName=fileName;
-        this.stat=stat;
+        this.statistics=statistics;
     }
 
     public String getFolder() {
@@ -22,11 +25,43 @@ public class FileSaver {
         return fileName;
     }
 
-    public HashMap<String, String> getStat() {
-        return stat;
+    public HashMap<String, HashMap<String,String >> getStatistics() {
+        return statistics;
     }
 
-    /*public void save(){
-        File file=new File(folder+System.sepa)
-    }*/
+    public void save(){
+        File stat=new File(folder+File.separator+fileName+".fasta");
+        if (stat.exists()){
+            System.out.println("File already exists");
+        } else {
+            try {
+                if (stat.createNewFile()) {
+                    System.out.println("File created successfully");
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+
+            try (FileWriter writer = new FileWriter(stat, false)) {
+                Set<String> files = statistics.keySet();
+
+                for (String file : files) {
+                    HashMap<String, String> sequences = statistics.get(file);
+
+                    Set<String> keys = sequences.keySet();
+
+                    writer.write(file + ":\n");
+
+                    for (String key : keys) {
+                        writer.write(key + " :: " + sequences.get(key) + "\n");
+                    }
+                }
+
+                writer.close();
+
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
 }
